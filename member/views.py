225e3552a92627707a089c3bf0c2cd671bf2b5bc
SUN <qiosion@gmail.com>
2023-06-02 10:51:27 +0900

    회원가입 기능 완성

--- member/views.py
+++ member/views.py
@@ -1,35 +1,33 @@
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-
-from member.forms import SignupForm
-
 
 # 회원가입
 def signup(request):
     if request.method == "GET":
-        # signupForm = UserCreationForm()
-        signupForm = SignupForm()
-        context = {'signupForm': signupForm}
+        # signupForm = SignupForm()
+        # context = {'signupForm': signupForm}
         return render(
             request,
             'member/signup.html',
-            context
         )
     elif request.method == "POST":
-        signupForm = SignupForm(request.POST)
-        # signupForm = UserCreationForm(request.POST)
-        if signupForm.is_valid():
-            signupForm.save()
-            # mbr = signupForm.save(commit=False)
-            # mbr.save()
+        first_name = request.POST.get('first_name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email1 = request.POST.get('email1')
+        email2 = request.POST.get('email2')
+        if first_name and username and password and email1 and email2:
+            email = f"{email1}@{email2}"
+            user = User(first_name=first_name, username=username, password=password, email=email)
+            user.save()
             return redirect('index')  # 회원가입 성공 시 메인으로 돌아감
         else:
             error_message = "폼이 유효하지 않습니다"
-            errors = signupForm.errors.as_data()
-            context = {'signupForm': signupForm, 'errors': errors}
+            context = {'error_message': error_message}
             return render(request, 'member/signup.html', context)
     return HttpResponse("Invalid request")  # POST 메서드 외에는 처리하지 않음
 

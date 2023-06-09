687cbe927cce509be2ae2986213809a2f9cc127e
SUN <qiosion@gmail.com>
2023-06-09 11:58:14 +0900

    게시글 조회-이미지 띄우기+다운로드

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-# from django.conf import settings
-# from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 import forum.views
 import member.views
+from mosaicImg.views import mosaic_download
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,6 +14,9 @@ urlpatterns = [
     path('forum/', include('forum.urls')), # 테스트용 게시판임
     path('board/', include('board.urls')),
     path('member/', include('member.urls')),
+    path('mosaic_download/<int:mos_no>/', mosaic_download, name='mosaic_download'),
 
 ]
               # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mosaicImg/views.py
+++ mosaicImg/views.py
@@ -9,9 +9,13 @@ from mosaicImg.models import MosaicImg
 def mosaic_download(request, mos_no):
     # mosaic_path = f"media/mosaic/{board_no}.jpg"
     mos = MosaicImg.objects.get(mos_no=mos_no)
-    board_no = mos.board_no
+    # board_no = mos.board_no
+    board_no = str(mos.board_no.board_no).zfill(8)
+    print('여기 : ', board_no)
     mosaic_path = os.path.join(settings.MEDIA_ROOT, 'mosaic', f'mosaic_{board_no}.jpg')
+    print('mosaic_path 는 ' , mosaic_path)
     mosaic_url = settings.MEDIA_URL + 'mosaic/' + f'mosaic_{board_no}.jpg'
+    print('mosaic_url 는 ' , mosaic_url)
 
     mos.mos_down = mosaic_path
     mos.save()

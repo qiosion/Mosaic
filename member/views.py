c27e92689c7fcb71365bc615aab8b3fab258e920
SUN <qiosion@gmail.com>
2023-09-06 16:36:13 +0900

    로그인 실패 메시지를 alert로 변경

--- member/views.py
+++ member/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import login as auth_login, authenticate
+from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
